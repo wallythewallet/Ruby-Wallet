765d1a722bd57cebffe6345d0e592abd0320b171
Max Mustermann <doomham@gmx.de>
2010-11-18 21:45:51 +0100

    added routes and menu

--- index.rb
+++ index.rb
@@ -3,9 +3,33 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'classes/Wallet.rb'
+require 'OTAPI'
+
+menu = [
+  {"name" => "Start", "link" => "/"},
+  {"name" => "Transfers", "link" => "/transfers"},
+  {"name" => "Cheques", "link" => "/cheques"},
+  {"name" => "Cash", "link" => "/cash"}
+  ]
 
 # Show start page
 get '/' do
-  @wallet = Wallet.new  
+  @wallet = Wallet.new
+  @menu = menu
   haml :index
+end
+
+get '/transfers' do
+  @menu = menu
+  haml :transfers
+end
+
+get '/cheques' do
+  @menu = menu
+  haml :cheques
+end
+
+get '/cash' do
+  @menu = menu
+  haml :cash
 end
